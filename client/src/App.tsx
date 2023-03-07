c971c668dc09d53e9302584ba293da37f124cf6f
Habeeb <habeebahmadu1@gmail.com>
2023-03-07 11:32:30 +0100

    feat: Setup router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ErrorPage, HomePage, QuizPage, ResultPage } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/quiz",
+    element: <QuizPage />,
+  },
+  {
+    path: "/result",
+    element: <ResultPage />,
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+]);
+
 function App() {
-  return <div className="bg-red-300 container mx-auto">Home</div>;
+  return (
+    <div className="container mx-auto">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
